78d6dd37d072e9c2ffebe839395e7da66575c905
adrobles <adariorobles.95@gmail.com>
2020-11-09 13:58:48 -0500

    Registro de tarea agregada al calendario

--- AMST1/app/src/main/java/com/example/amst1/calendario.java
+++ AMST1/app/src/main/java/com/example/amst1/calendario.java
@@ -2,6 +2,7 @@ package com.example.amst1;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,16 +13,36 @@ import android.widget.CalendarView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class calendario extends AppCompatActivity {
     CalendarView calendarView;
     TextView myDate;
     Button btnSalir;
+    Calendar calendar;
+    //private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMM- yyy", Locale.getDefault());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+
         btnSalir = (Button) findViewById(R.id.btnSalir);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendar = Calendar.getInstance();
+        calendar.set(Calendar.MONTH, Calendar.NOVEMBER);
+        calendar.set(Calendar.DAY_OF_MONTH, 20);
+        calendar.set(Calendar.YEAR, 2020);
+
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
         btnSalir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,15 +50,24 @@ public class calendario extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        calendarView = (CalendarView) findViewById(R.id.calendarView);
-        myDate = (TextView) findViewById(R.id.myDate);
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = (month + 1) + "/" + (dayOfMonth) + "/" + (year);
-                myDate.setText(date);
+                if (year == 2020 & month == 10 & dayOfMonth == 20){
+                    Toast toast=Toast.makeText(getApplicationContext(),"Clases de AMST",Toast.LENGTH_SHORT);
+                    toast.show();
+                }else {
+                    Toast toast=Toast.makeText(getApplicationContext(),"Nada programado para esta fecha",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
+
+
+
+
+
 
 
     }
