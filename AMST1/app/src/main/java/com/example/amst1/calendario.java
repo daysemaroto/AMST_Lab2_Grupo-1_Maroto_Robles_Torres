65be5e273019162b951b8ab81a455fb7bb8ae54d
adrobles <adariorobles.95@gmail.com>
2020-11-08 19:23:48 -0500

    act

--- AMST1/app/src/main/java/com/example/amst1/calendario.java
+++ AMST1/app/src/main/java/com/example/amst1/calendario.java
@@ -1,5 +1,6 @@
 package com.example.amst1;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,33 +10,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class calendario extends AppCompatActivity {
     CalendarView calendarView;
     TextView myDate;
     Button btnSalir;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
-        btnSalir = (Button)findViewById(R.id.btnSalir);
+        btnSalir = (Button) findViewById(R.id.btnSalir);
         btnSalir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
             }
         });
-        calendarView = (CalendarView)findViewById(R.id.calendarView);
-        myDate = (TextView)findViewById(R.id.myDate);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = (month+1)+"/"+(dayOfMonth)+"/"+(year);
+                String date = (month + 1) + "/" + (dayOfMonth) + "/" + (year);
                 myDate.setText(date);
             }
         });
+
+
     }
 }
